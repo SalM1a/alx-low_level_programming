1f9348d0adf71b93368ccb2545f804fa62416692
SalM1a <salma.abodokhan@gmail.com>
2023-10-11 07:56:59 -0700

    dog_t

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,6 +14,10 @@ float age;
 char *owner;
 };
 
+/**
+ * dog_t - Typedef for dog_t
+ */
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
