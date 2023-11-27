2c1f45a3bdc43984328253b128ed463cd835c377
SalM1a <salma.abodokhan@gmail.com>
2023-11-27 14:16:50 -0800

    create

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * create_file -  a function that creates a file
+ * @filename: the name of the file to create
+ * @text_content: is a NULL terminated string to write to the file
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+int fd;
+size_t w;
+size_t sz = 0;
+
+if (filename == NULL)
+return (-1);
+
+if (text_content == NULL)
+fd = open(filename, O_CREAT | O_RDWR | O_TRUNC | 0600);
+if (fd == -1)
+return (-1);
+
+while (text_content[sz] != '\0')
+{
+w = write(fd, text_content, sz);
+sz++;
+if (w != sz)
+{
+return (-1);
+}
+}
+close(fd);
+return (1);
+}
