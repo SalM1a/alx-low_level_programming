3796d46a1950fcc7be7211c79d42fa6963724d33
SalM1a <salma.abodokhan@gmail.com>
2023-11-27 18:56:02 -0800

    24

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -24,7 +24,7 @@ int append_text_to_file(const char *filename, char *text_content)
 int fd;
 ssize_t size = 0;
 size_t len;
-if (filename == NULL || text_content == NULL)
+if (filename == NULL)
 return (-1);
 
 fd = open(filename, O_WRONLY | O_APPEND);
