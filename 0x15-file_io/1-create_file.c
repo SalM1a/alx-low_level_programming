5bd0fb8a4f7fc66a1a6eacf14d91c6b2f3bc7bed
SalM1a <salma.abodokhan@gmail.com>
2023-11-27 16:24:47 -0800

    text_content

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,5 +1,19 @@
 #include "main.h"
 /**
+ * _strlen - a function that return the length of string
+ * @str: string whose length to be printed
+ * Return: length of string
+ */
+int _strlen(char *str)
+{
+int i = 0;
+while (str[i] != '\0')
+{
+i++;
+}
+return(str[i]);
+}
+/**
  * create_file -  a function that creates a file
  * @filename: the name of the file to create
  * @text_content: is a NULL terminated string to write to the file
@@ -8,21 +22,22 @@
 int create_file(const char *filename, char *text_content)
 {
 int fd;
-size_t w;
-size_t sz = 0;
+ssize_t w;
+ssize_t sz = 0;
 
 if (filename == NULL)
 return (-1);
 
 if (text_content == NULL)
-fd = open(filename, O_CREAT | O_RDWR | O_TRUNC | 0600);
+text_content = " ";
+fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR);
 if (fd == -1)
 return (-1);
 
-while (text_content[sz] != '\0')
+if (text_content != NULL)
 {
+sz = _strlen(text_content);
 w = write(fd, text_content, sz);
-sz++;
 if (w != sz)
 {
 return (-1);
